46c8874f2e3540531e526567ddd8d23c3a4ef16f
Paweł Zielonka <lite.what@gmail.com>
2020-06-17 21:35:38 +0200

    Composing ui components. Passing data by props

--- src/main/app/ui.cljs
+++ src/main/app/ui.cljs
@@ -3,32 +3,49 @@
             [com.fulcrologic.fulcro.dom :as dom]))
 
 (defsc ClickCounter
-       [this {:keys [:counter/clicks :counter/name]}]
-       {}
-       (js/console.log this)
-       (dom/div {:id (str "ClickCounter-" name)}
-                (dom/h1 "Counter")
-                (dom/p "My name is " name)
-                (dom/p (str "Clicked: " clicks " times"))))
+  [this {:keys [:counter/clicks :counter/name]}]
+  {}
+  (js/console.log this)
+  (dom/div {:id (str "ClickCounter-" name)}
+    (dom/h1 "Counter")
+    (dom/p "My name is " name)
+    (dom/p (str "Clicked: " clicks " times"))))
 
 (defsc Person
-       [this {:keys [:person/name :person/age]}]
-       {}
-       (dom/div
-         (dom/h3 "Person")
-         (dom/p
-           (dom/span (str "Name: " name))
-           (dom/br)
-           (dom/span (str "Age: " age)))))
+  [this {:keys [:person/name :person/age]}]
+  {}
+  (dom/li
+    (dom/h5 (str name " " "(age: " age ")"))))
 
-(def ui-person (comp/factory Person))
+(def ui-person (comp/factory Person {:keyfn :person/name}))
+
+(defsc PersonList
+  [this {:keys [:list/label :list/people]}]
+  {}
+  (dom/div
+    (dom/h4 label)
+    (dom/ul
+      (map ui-person people))))
+
+(def ui-person-list (comp/factory PersonList))
 
 (defsc Root
-       [this props]
-       {}
-       (js/console.log "Props of Root:" (comp/props this))
-       (dom/div :.container
-                (dom/h1 "Root component")
-                (dom/p "I am the Root component!")
-                (dom/div
-                  (ui-person {:person/name "Paweł" :person/age 28}))))
+  [this {:keys [ui/react-key]}]
+  {}
+  (let [ui-data {:friends {:list/label  "Friends"
+                           :list/people [{:person/name "Andrew"
+                                          :person/age  26}
+                                         {:person/name "Diana"
+                                          :person/age  18}]}
+                 :enemies {:list/label  "Enemies"
+                           :list/people [{:person/name "Jonathan"
+                                          :person/age  45}
+                                         {:person/name "Daren"
+                                          :person/age  25}]}}]
+    (js/console.log "Root" this)
+    (js/console.log "Props of Root:" (comp/props this))
+    (dom/div :.container
+      (dom/h1 "Root component")
+      (dom/div
+        (ui-person-list (:friends ui-data))
+        (ui-person-list (:enemies ui-data))))))
