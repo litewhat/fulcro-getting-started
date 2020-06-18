28bc1184d7081959557d2e813230a0b3175d7e64
Paweł Zielonka <lite.what@gmail.com>
2020-06-18 10:10:13 +0200

    Server handler

---
+++ src/main/app/server.clj
@@ -0,0 +1,31 @@
+(ns app.server
+  (:require [com.fulcrologic.fulcro.server.api-middleware :as api-middleware]
+            [org.httpkit.server :as http]
+            [ring.middleware.content-type :refer [wrap-content-type]]
+            [ring.middleware.resource :refer [wrap-resource]]
+            [app.parser :refer [api-parser]]))
+
+(def ^:private not-found-handler
+  (fn [req]
+    {:status 404
+     :headers {"Content-Type" "text/plain"}
+     :body "Not Found"}))
+
+(def app-handler
+  (-> not-found-handler
+      (api-middleware/wrap-api {:uri "/api"
+                                :parser api-parser})
+      (api-middleware/wrap-transit-params)
+      (api-middleware/wrap-transit-response)
+      (wrap-resource "public")
+      (wrap-content-type)))
+
+(defonce server (atom nil))
+
+(defn start []
+  (reset! server (http/run-server app-handler {:port 3000})))
+
+(defn stop []
+  (when @server
+    (@server)
+    (reset! server nil)))
