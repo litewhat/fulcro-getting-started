c5b7f05520c72104b469861cf0bd6dd0eee5f04f
Paweł Zielonka <lite.what@gmail.com>
2020-07-15 01:02:30 +0200

    Separated mocking functions

--- test/app/user_registration/model_test.cljs
+++ test/app/user_registration/model_test.cljs
@@ -1,52 +1,10 @@
 (ns app.user-registration.model-test
   (:require [cljs.test :refer [deftest testing is]]
-            [app.user-registration.model :as sut]))
-
-(def default-state-map
-  {:error/id
-   {#uuid"2630743f-8d83-4e14-a577-3493dad4c802"
-    {:error/code       :invalid-email
-     :error/message    "Invalid email"
-     :error/field-name :user-registration/email
-     :error/id         #uuid"2630743f-8d83-4e14-a577-3493dad4c802"}
-    #uuid"9b496605-9371-4c9e-9ab3-b7824e5af59f"
-    {:error/code       :invalid-password
-     :error/message    "Password should contain minimum 8 characters, lowercase letter, uppercase letter and number"
-     :error/field-name :user-registration/password
-     :error/id         #uuid"9b496605-9371-4c9e-9ab3-b7824e5af59f"}
-    #uuid"c5d4f45d-524c-4542-b702-4b1e02a62d69"
-    {:error/code       :invalid-confirm-password
-     :error/message    "Password does not match"
-     :error/field-name :user-registration/confirm-password
-     :error/id         #uuid"c5d4f45d-524c-4542-b702-4b1e02a62d69"}
-    #uuid"fa8da194-7105-43ae-aaaa-0634ebea094d"
-    {:error/code       :invalid-password
-     :error/message    "Password should contain minimum 8 characters, lowercase letter, uppercase letter and number"
-     :error/field-name :user-registration/password
-     :error/id         #uuid"fa8da194-7105-43ae-aaaa-0634ebea094d"}}
-   :user-registration
-   [:user-registration/id #uuid"3f65cf59-4049-4ed4-be1e-f51cc93e331c"]
-   :user-registration/id
-   {#uuid"3f65cf59-4049-4ed4-be1e-f51cc93e331c"
-    {:user-registration/id               #uuid"3f65cf59-4049-4ed4-be1e-f51cc93e331c"
-     :user-registration/errors           [[:error/id #uuid"2630743f-8d83-4e14-a577-3493dad4c802"]
-                                          [:error/id #uuid"9b496605-9371-4c9e-9ab3-b7824e5af59f"]
-                                          [:error/id #uuid"c5d4f45d-524c-4542-b702-4b1e02a62d69"]]
-     :user-registration/email            "adsasdasd"
-     :user-registration/password         "asdasd"
-     :user-registration/confirm-password "asdasd"}
-    #uuid"b7a51278-1a10-48da-b091-062188c90eea"
-    {:user-registration/id               #uuid"b7a51278-1a10-48da-b091-062188c90eea"
-     :user-registration/errors           [[:error/id #uuid"fa8da194-7105-43ae-aaaa-0634ebea094d"]]
-     :user-registration/email            "test@example.com"
-     :user-registration/password         "asdasd"
-     :user-registration/confirm-password "asdasd"}}})
-
-(defn mock-env [state-map]
-  {:state (atom state-map)})
+            [app.user-registration.model :as sut]
+            [app.test.mock :as mock]))
 
 (deftest list-errors-test
-  (let [env    (mock-env default-state-map)
+  (let [env    (mock/env)
         errors (sut/list-errors env #uuid"3f65cf59-4049-4ed4-be1e-f51cc93e331c")]
     (is (= 3 (count errors)))
     (is (= #{#uuid"2630743f-8d83-4e14-a577-3493dad4c802"
