0441a4d94df386a8d8898c51e11d9211d73dab6e
Paweł Zielonka <lite.what@gmail.com>
2020-07-15 14:00:59 +0200

    Generating uuid4 by default for app_user record.

--- src/main/app/user/db/queries.sql
+++ src/main/app/user/db/queries.sql
@@ -4,8 +4,8 @@
 -- :result :raw
 -- :doc Create app_user table
 create table app_user (
-    id          uuid primary key,
-    email       varchar(255) not null,
+    id          uuid default uuid_generate_v4() primary key,
+    email       varchar(255) not null unique,
     created_at  timestamp not null default current_timestamp,
     deleted_at  timestamp null
 );
@@ -15,3 +15,11 @@ create table app_user (
 -- :result :raw
 -- :doc Drop user table
 drop table if exists app_user restrict;
+
+-- :name insert-app-user
+-- :command :returning-execute
+-- :result :one
+-- :doc Insert person record
+insert into app_user (id, email)
+    values (default, :email)
+    returning id;
