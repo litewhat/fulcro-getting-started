1dbdae3df54169582c2b54a7d1fabd50d1234256
Paweł Zielonka <lite.what@gmail.com>
2020-07-16 01:27:26 +0200

    Updated tests for db setup and teardown

--- test/app/db_test.clj
+++ test/app/db_test.clj
@@ -7,13 +7,25 @@
   [db-conn]
   (map :tablename (hc/db-run db-conn "SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';")))
 
-(deftest ^:integration db-connection-test
-  (testing "all tables exist after setup"
-    (db/set-up-tables! db/conn-spec)
-    (let [tables (set (list-table-names db/conn-spec))]
-      (is (= #{"person" "person_list" "person_list_people" "app_user"} tables))))
+(defn list-data-types
+  [db-conn]
+  (map :type (hc/db-run db-conn "SELECT n.nspname as schema, t.typname as type FROM pg_type t LEFT JOIN pg_catalog.pg_namespace n ON n.oid = t.typnamespace WHERE (t.typrelid = 0 OR (SELECT c.relkind = 'c' FROM pg_catalog.pg_class c WHERE c.oid = t.typrelid)) AND NOT EXISTS(SELECT 1 FROM pg_catalog.pg_type el WHERE el.oid = t.typelem AND el.typarray = t.oid) AND n.nspname NOT IN ('pg_catalog', 'information_schema');")))
 
-  (testing "no table exists after tear down"
-    (db/tear-down-tables! db/conn-spec)
+(deftest ^:integration db-connection-test
+  (testing "all tables and data types exist after setup"
+    (db/set-up-tables! db/conn-spec)
+
     (let [tables (set (list-table-names db/conn-spec))]
-      (is (empty? tables)))))
+      (is (= #{"person" "person_list" "person_list_people" "app_user" "token"} tables)))
+
+    (let [data-types (set (list-data-types db/conn-spec))]
+      (is (= #{"token_type"} data-types))))
+
+  (testing "no table and data types exists after tear down"
+    (db/tear-down-tables! db/conn-spec)
+
+    (let [tables (set (list-table-names db/conn-spec))]
+      (is (empty? tables)))
+
+    (let [data-types (set (list-data-types db/conn-spec))]
+      (is (empty? data-types)))))
