21eab0cc93b5770e81ff27d1ca7dc0a41fab34a0
Paweł Zielonka <lite.what@gmail.com>
2020-07-16 01:26:08 +0200

    Helper query for enum types

--- src/main/app/db/queries.sql
+++ src/main/app/db/queries.sql
@@ -10,3 +10,15 @@ CREATE EXTENSION :identifier:name;
 -- :result :raw
 -- :doc Drop extension
 DROP EXTENSION IF EXISTS :identifier:name RESTRICT;
+
+-- :name get-all-enum-type-values
+-- :command :query
+-- :result :many
+-- :doc List all values associated with given type name
+SELECT n.nspname AS enum_schema,
+       t.typname AS enum_name,
+       e.enumlabel AS enum_value
+FROM pg_type t
+   JOIN pg_enum e ON t.oid = e.enumtypid
+   JOIN pg_catalog.pg_namespace n ON n.oid = t.typnamespace
+where t.typname = /*~ (format "'%s'" (:name params)) ~*/;
